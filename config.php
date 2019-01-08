ba409caf1c0bd6359392f02a1d07e4b6391cadf2
Guilherme Ferreira <guilherme.ferreira29@hotmail.com>
2019-01-07 22:00:16 -0200

    Implementação da classe Usuario

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Busca automaticamente as classes necessarias que estão na raiz
 spl_autoload_register(function($class_name){
     // concatena o nome da classe com o .php
-    $filename = $class_name . ".php";
+    $filename = "class" . DIRECTORY_SEPARATOR .$class_name . ".php";
     // caso exista o arquivo o mesmo é chamado
     if (file_exists(($filename))) {
         # code...
--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 require_once("config.php");
 
-$sql = new Sql();
-$usuarios = $sql->select("select * from tb_usuario");
-
-echo json_encode($usuarios);
+$root = new Usuario();
+$root ->loadId(5);
 
 ?>
